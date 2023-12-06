df25f44534c7b0fb8786e559a9ff3cba899210b0
Argo Roots <argo@roots.ee>
2023-12-06 16:15:33 +0200

    Add Plausible

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: { lang: 'en' },
+      script: [
+        { src: 'https://plausible.io/js/script.js', 'data-domain': 'inkbird.roots.ee', defer: true }
+      ]
+    }
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
